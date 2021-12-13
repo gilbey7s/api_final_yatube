f0503ae5bf96e9ea2b02e5aec6319c27eca4936f
gilbey7s <s.l.a.y.e.r.energy@yandex.ru>
2021-12-13 14:05:11 +0300

    for review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post, Follow
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer, FollowSerializer
+from posts.models import Group, Post, Follow
 
 
 class PostViewSet(viewsets.ModelViewSet):
